b8d32877b3d4ec15722e1c644ab1aab51102dccf
naveen9979 <sathyapriyak2020@gmail.com>
2024-08-07 00:11:00 -0700

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,53 @@
+public class assignment {
+    public static void main(String[]args){
+        int X = 23;
+        System.out.println(X);
+
+        int y = 100;
+        y += 10;
+        System.out.println(y);
+
+        int z = 5;
+        z -= 2;
+        System.out.println(z);
+
+        int o = 10;
+        o *= 3;
+        System.out.println(o);
+
+
+        int k = 899;
+        k /= 8;
+        System.out.println(k);
+
+
+        int g = 7879;
+        g  %=  4;
+        System.out.println(g);
+
+
+        int n = 900;
+        n &= 234;
+        System.out.println(n);
+
+        int m = 5647;
+        m |= 566;
+        System.out.println(m);
+
+      
+        int b = 787789;
+        b ^= 6778;
+        System.out.println(b);
+
+
+        int f = 243;
+        int t = 45;
+        System.out.println(f >>= t);
+
+
+        int h = 67777;
+        int d = 4567;
+        System.out.println(h <<= d);
+    }
+    
+}
